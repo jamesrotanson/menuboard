7362d2ffa693a4a9cb949a08ca504a74cdcb194a
jamesrotanson <jamesrotanson@gmail.com>
2023-02-11 13:12:56 +1100

    Added options to personalisation onboarding

--- src/components/Bubble.js
+++ src/components/Bubble.js
@@ -2,12 +2,17 @@ import React from 'react'
 import Twemoji from 'react-twemoji'
 
 const Bubble = (props) => {
+  
+  const handleSelect = (event) => {
+    event.currentTarget.classList.toggle('Bubble-option-active')
+  }
+
   return (
-    <li className='Bubble-option'>
+    <li className='Bubble-option' onClick={handleSelect}>
         <Twemoji options={{className: 'Twemoji-small'}}>
             <p>{props.icon}</p>
         </Twemoji>
-        <h5>{props.name}</h5>
+        <p>{props.name}</p>
     </li>
   )
 }
